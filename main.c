a57d41f9a733de425642fd1d78fd2d8c92819774
isaacte <isaac@ite2002.net>
2022-03-17 12:45:48 +0100

    Edit

--- main.c
+++ main.c
@@ -12,44 +12,103 @@ void read_matrix_from_file(char filename[], int rows, int columns, int matrix[ro
     }
 }
 
-void transpose_matrix(int rows, int columns, int original[rows][columns], int transposed[columns][rows]) {
-    for (int i = 0; i < rows; i++) {
+void transpose_matrix(int rows, int columns, int original[rows][columns], int transposed[columns][rows], FILE *fitxer) {
+    for (int i = 0; i < rows; i++){
+        fprintf(fitxer,"%d %p\n",2,&i); //:)
         for (int j = 0; j < columns; j++) {
+            fprintf(fitxer,"%d %p\n",2,&j); //:)
             transposed[j][i] = original[i][j];
+            fprintf(fitxer,"%d %p\n",3,&transposed[j][i]); //:)
+            fprintf(fitxer,"%d %p\n",2,&original[i][j]); //:)
+            fprintf(fitxer,"%d %p\n",3,&j);
         }
+        fprintf(fitxer,"%d %p\n",3,&i);
     }
 }
 
 void matrix_product(int rowsA, int columnsA, int rowsB, int columnsB, int A[rowsA][columnsA], int B[rowsB][columnsB],
-                    int res[rowsA][columnsB]) {
+                    int res[rowsA][columnsB], FILE *fitxer) {
     if (rowsB == columnsA) { //Check if the operation can be done
         for (int i = 0; i < columnsB; i++){
+            fprintf(fitxer,"%d %p\n",2,&i); //:)
             for (int j = 0; j< rowsA; j++){
+                fprintf(fitxer,"%d %p\n",2,&j); //:)
                 res[j][i] = 0;
+                fprintf(fitxer,"%d %p\n",3, &res[j][i]); //:)
+
                 for (int k = 0; k < columnsA; k++){
+                    fprintf(fitxer,"%d %p\n",2,&k); //:)
+
+
                     res[j][i] += A[j][k] * B[k][i];
+                    fprintf(fitxer,"%d %p\n",2,&A[j][k]); //:)
+                    fprintf(fitxer,"%d %p\n",2,&B[k][i]); //:)
+                    fprintf(fitxer,"%d %p\n",3,&res[j][i]); //:)
+
+
+                    fprintf(fitxer,"%d %p\n",3,&k); //:)
+
                 }
+                fprintf(fitxer,"%d %p\n",3,&j); //:)
+
             }
+            fprintf(fitxer,"%d %p\n",3,&i); //:)
         }
     }
 }
 
-void opposite_matrix(int rows, int columns, int matrix[rows][columns], int res[rows][columns]){
+void opposite_matrix(int rows, int columns, int matrix[rows][columns], int res[rows][columns], FILE *fitxer){
     for (int i = 0; i < rows; i++){
-        for (int j = 0; i < columns; j++){
-            res[i][j] = - matrix[i][j];
+        fprintf(fitxer,"%d %p\n",2,&i); //:)
+        for (int j = 0; j < columns; j++){
+            fprintf(fitxer,"%d %p\n",2,&j); //:)
+            res[i][j] = 0 - matrix[i][j];
+            fprintf(fitxer,"%d %p\n",2,&matrix[i][j]); //:)
+            fprintf(fitxer,"%d %p\n",3,&res[i][j]); //:)
+
+            fprintf(fitxer,"%d %p\n",3,&j); //:)
         }
+        fprintf(fitxer,"%d %p\n",3,&i); //:)
+
     }
 }
 
-//Check if matrix is equal
+void check_equals(int rows, int columns, int A[rows][columns], int B[rows][columns], FILE *fitxer){
+    int equal = 1;
+    for (int i = 0; i < rows; i++){
+        fprintf(fitxer,"%d %p\n",2,&i); //:)
+        for (int j = 0; j < columns; j++){
+            fprintf(fitxer,"%d %p\n",2,&j); //:)
+            if (A[i][j] == B[j][i]){
+                fprintf(fitxer,"%d %p\n",2,&B[j][i]); //:)
+                fprintf(fitxer,"%d %p\n",3,&A[i][j]); //:)
 
-void print_matrix(int rows, int columns, int matrix[rows][columns]) {
+                equal = 0;
+                break;
+            }
+            fprintf(fitxer,"%d %p\n",3,&j); //:)
+        }
+        fprintf(fitxer,"%d %p\n",3,&i); //:)
+
+    }
+    if (equal == 0){
+        printf("No són iguals");
+    } else {
+        printf("Són iguals");
+    }
+}
+void print_matrix(int rows, int columns, int matrix[rows][columns], FILE *fitxer) {
     for (int i = 0; i < rows; i++) {
+        fprintf(fitxer,"%d %p\n",2,&i); //:)
         for (int j = 0; j < columns; j++) {
+            fprintf(fitxer,"%d %p\n",2,&j); //:)
             printf("%i ", matrix[i][j]);
+
+            fprintf(fitxer,"%d %p\n",3,&j); //:)
         }
         printf("\n");
+        fprintf(fitxer,"%d %p\n",3,&i); //:)
+
     }
 }
 
